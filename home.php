002f2e13822ec7442c9274b1a1838c182b466973
drelocatelli <andressalocatelli@hotmail.com>
2023-06-22 19:20:01 -0300

    fix mysql connection

--- home.php
+++ home.php
@@ -1,5 +1,4 @@
 <?php require_once('includes/header.php'); ?>
-
 <div style="clear:both;"></div>
 
 <div id="principal">
--- src/mysqlConnect.php
+++ src/mysqlConnect.php
@@ -1,7 +1,6 @@
 <?php
-
-$conn = new mysqli("localhost", "root", "123456", "adotapet");
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+$conn = new mysqli(getenv("DB_HOST"), getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_NAME"), getenv("DB_PORT"));
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
     exit();
 }
